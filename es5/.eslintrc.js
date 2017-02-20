f9bb712b7f3361e9cdbb1c79adfb8e02b63d0652
jserz <rnjnfns8@126.com>
2017-02-20 18:35:50 +0800

    完善 es5

--- es5/.eslintrc.js
+++ es5/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     // 分号
     // 语句结束一定要加分号
     'semi': ['error', 'always'],
-    // 避免不必要的分号
+    // 避免不必要的分号，`if-else`、`while`、`for`、`for-in`、`try-catch-finally`、函数声明、等的代码块后面不需要分号
     'no-extra-semi': 'error',
     // 在分号后面加空格，行末的分号除外
     'semi-spacing': ['error', {'before': false, 'after': true}]
